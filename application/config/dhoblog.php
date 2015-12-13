00042643b87eab6d133a00acb6d400c046df5d13
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-12-13 17:05:12 +0700

    add new config related to multisite

--- application/config/dhoblog.php
+++ application/config/dhoblog.php
@@ -15,7 +15,6 @@ $config['author_email'] = 'ridho@mutasimridlo.com';
 $config['version_check_url'] = 'https://update.dhosoft.com/dhoblog/';
 $config['website_url'] = 'https://www.dhosoft.com/dhoblog/';
 $config['documentation_url'] = 'https://docs.dhosoft.com/dhoblog/';
-$config['bugtracker_url'] = 'http://bugs.dhosoft.com/dhoblog/';
 $config['download_url'] = 'https://www.dhosoft.com/dhoblog/downloads/';
 
 /*
@@ -23,7 +22,37 @@ $config['download_url'] = 'https://www.dhosoft.com/dhoblog/downloads/';
 | Multisite
 |--------------------------------------------------------------------------
 |
-| Whether use multisite or not
+| Whether allow multisite or not
 |
 */
-$config['multisite']='false';
+$config['allow_multisite']='false';
+
+/*
+|--------------------------------------------------------------------------
+| Use Domain
+|--------------------------------------------------------------------------
+|
+| Whether use domain or not, your sub blog will be domain.tld
+|
+*/
+$config['use_domain']='false';
+
+/*
+|--------------------------------------------------------------------------
+| Use Subdomain
+|--------------------------------------------------------------------------
+|
+| Whether use subdomain or not, your sub blog will be sub.domain.tld
+|
+*/
+$config['use_subdomain']='false';
+
+/*
+|--------------------------------------------------------------------------
+| Use Subdirectory
+|--------------------------------------------------------------------------
+|
+| Whether use subdirectory or not, your sub blog will be domain.tld/sub
+|
+*/
+$config['use_subdirectory']='false';
