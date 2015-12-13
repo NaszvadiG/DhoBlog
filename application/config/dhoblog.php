e546be7eddabd47ae932adefa0e74b2494ea412a
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-12-13 14:56:41 +0700

    add dhoblog informations

--- application/config/dhoblog.php
+++ application/config/dhoblog.php
@@ -1,4 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/*
+|--------------------------------------------------------------------------
+| dhoBlog Information
+|--------------------------------------------------------------------------
+|
+| Informations related with dhoBlog
+|
+*/
+$config['version'] = '1.0.0';
+$config['author'] = 'Mutasim Ridlo, S.Kom';
+$config['author_email'] = 'ridho@mutasimridlo.com';
+$config['version_check_url'] = 'https://update.dhosoft.com/dhoblog/';
+$config['website_url'] = 'https://www.dhosoft.com/dhoblog/';
+$config['documentation_url'] = 'https://docs.dhosoft.com/dhoblog/';
+$config['bugtracker_url'] = 'http://bugs.dhosoft.com/dhoblog/';
+$config['download_url'] = 'https://www.dhosoft.com/dhoblog/downloads/';
+
+/*
+|--------------------------------------------------------------------------
+| Multisite
+|--------------------------------------------------------------------------
+|
+| Whether use multisite or not
+|
+*/
 $config['multisite']='false';
