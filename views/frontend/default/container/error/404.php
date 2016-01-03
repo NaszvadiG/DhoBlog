d37acaf6efb812d339e9ce1e9373478a7c33f985
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2016-01-03 18:47:43 +0700

    added 404 error page

---
+++ views/frontend/default/container/error/404.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="col-md-9 ">
+       <div class="blog-header">
+        <h1 class="blog-title"><?php echo $blog_title;?></h1>
+        <p class="lead blog-description"><?php echo $blog_tagline;?></p>
+      </div>
+      <div class="blog-main">
+        <div class="blog-post">
+        <h3 class="blog-post-title">Page not found!</h3>
+        <p>The page you looking for is doesn't exist!</p>
+        </div>
+    </div>
+
+</div>
